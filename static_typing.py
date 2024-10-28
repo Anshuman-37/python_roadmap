605373980a8dec56ae5968b54751d3017095c140
Anshuman Singh <asingh99cool@gmail.com>
2024-10-28 07:55:00 +0530

    static typing examples

---
+++ static_typing.py
@@ -0,0 +1,8 @@
+# Define function with type hints for parameters and return type
+def add_numbers(a: int, b: int) -> int:
+    return a + b
+
+# Variable with type hint
+count: int = 10
+
+print(count)
